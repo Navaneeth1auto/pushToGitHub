508ebe136673113daa00ec954ff205f5e4af41be
navaneeth <navaneeth.info92@gmail.com>
2019-09-21 09:45:10 +0530

    pushing the initial project

---
+++ src/main/java/Automation/pushingToGit/App.java
@@ -0,0 +1,13 @@
+package Automation.pushingToGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
